5a06e4daa3a9b6d5db13e078ef6876bbad41b8d4
Aaron Janes <Aaron.Janes@gmail.com>
2021-04-17 13:38:16 -0600

    implamented ngFor

--- src/app/components/users/users.component.ts
+++ src/app/components/users/users.component.ts
@@ -1,15 +1,66 @@
 import { Component, OnInit } from '@angular/core';
+import { User } from 'src/app/models/User';
 
 @Component({
   selector: 'app-users',
   templateUrl: './users.component.html',
-  styleUrls: ['./users.component.scss']
+  styleUrls: ['./users.component.scss'],
 })
 export class UsersComponent implements OnInit {
+  users: User[];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
+  ngOnInit() {
+    this.users = [
+      {
+        firstName: 'Rob',
+        lastName: 'Mathews',
+        age: 45,
+        address: {
+          street: '516512 Fowens Drive',
+          city: 'Baddeck',
+          province: 'Nova Scotia',
+          postalCode: 'B0E1B0',
+        },
+      },
+      {
+        firstName: 'Kevin',
+        lastName: 'Johnson',
+        age: 91,
+        address: {
+          street: ' 5651 Crown Drive',
+          city: 'Toronto',
+          province: 'Ont',
+          postalCode: 'B0E1B0',
+        },
+      },
+      {
+        firstName: 'Tristan',
+        lastName: 'Dumbey',
+        age: 25,
+        address: {
+          street: '9451621 mills Drive',
+          city: 'Quebec',
+          province: 'QC',
+          postalCode: 'B0E1B0',
+        },
+      },
+    ];
+    this.addUser({
+      firstName: 'David',
+      lastName: 'Jackson ',
+      age: 99,
+      address: {
+        street: '9451621 mills Drive',
+        city: 'Calgary',
+        province: 'AB',
+        postalCode: 'B0E1B0',
+      },
+    });
   }
 
+  addUser(user: User) {
+    this.users.push(user);
+  }
 }
