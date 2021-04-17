037ba8bb5db9bab893bb1c7f8ef0b475819c00b7
Aaron Janes <Aaron.Janes@gmail.com>
2021-04-17 14:49:41 -0600

    add bootstrap and set up nav bar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,12 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { UserComponent } from './components/user/user.component';
 import { UsersComponent } from './components/users/users.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { NavbarComponent } from './components/navbar/navbar.component';
 
 @NgModule({
-  declarations: [AppComponent, UserComponent, UsersComponent],
-  imports: [BrowserModule, AppRoutingModule],
+  declarations: [AppComponent, UserComponent, UsersComponent, NavbarComponent],
+  imports: [BrowserModule, AppRoutingModule, NgbModule],
   providers: [],
   bootstrap: [AppComponent],
 })
