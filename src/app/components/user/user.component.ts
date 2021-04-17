9e521bf6d7e8fe625c7412d7038bf14e481c0250
Aaron Janes <Aaron.Janes@gmail.com>
2021-04-17 12:22:59 -0600

    class methods

--- src/app/components/user/user.component.ts
+++ src/app/components/user/user.component.ts
@@ -4,12 +4,26 @@ import { Component } from '@angular/core';
   selector: 'app-user',
   templateUrl: './user.component.html',
   styleUrls: ['./user.component.style.scss'],
-  // styles: [
-  //   `
-  //     h2 {
-  //       color: blue;
-  //     }
-  //   `,
-  // ],
 })
-export class UserComponent {}
+export class UserComponent {
+  // properties
+  firstName = 'John';
+  lastName = ' Doe';
+  age = 40;
+
+  //Methods that are run at the time the instance is created
+  constructor() {
+    this.sayHello();
+    console.log(this.age);
+    this.hasBirthday();
+    console.log(this.age);
+  }
+
+  sayHello() {
+    console.log(`Hello ${this.firstName}`);
+  }
+
+  hasBirthday() {
+    this.age += 1;
+  }
+}
