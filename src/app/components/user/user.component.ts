3ba74416bd4df81c7df4109c6ba36e6de09ee53c
Aaron Janes <Aaron.Janes@gmail.com>
2021-04-17 12:14:14 -0600

    set up first Angular class

---
+++ src/app/components/user/user.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-user',
+  templateUrl: './user.component.html',
+  styleUrls: ['./user.component.style.scss'],
+  // styles: [
+  //   `
+  //     h2 {
+  //       color: blue;
+  //     }
+  //   `,
+  // ],
+})
+export class UserComponent {}
